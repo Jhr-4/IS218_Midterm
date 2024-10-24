30d303e1158392123063d8edd7367816c147f692
Jay <jhr4@njit.edu>
2024-10-24 18:17:45 -0400

    basic logging implemented

--- app/__init__.py
+++ app/__init__.py
@@ -2,28 +2,51 @@ import os
 import importlib
 from app.commands import CommandHandler
 from app.commands import Command
-
+import logging
+import logging.config
 
 class App:
     def __init__(self):
+        os.makedirs('logs', exist_ok=True)
+        self.configure_logging()
         self.commandHandler = CommandHandler()
 
-    def start(self):
+    def configure_logging(self):
+        logging_conf_path = 'logging.conf'
+        if os.path.exists(logging_conf_path):
+            logging.config.fileConfig(logging_conf_path, disable_existing_loggers=False)
+        else:
+            logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.info("Logging configured.")
 
+    def enable_plugins(self):
         plugins_pkg = 'app.plugins'
         plugins_path = 'app/plugins'
+        
+        if plugins_path is None:
+            logging.error("Plugins Directory Not Found: " + plugins_path)
+            return
 
-        for folder in os.listdir(plugins_path):
-            if os.path.isdir(f"{plugins_path}/{folder}"): 
-                plugin_module = importlib.import_module(f"{plugins_pkg}.{folder}")
-                for itemName in dir(plugin_module):
-                    item = getattr(plugin_module, itemName)
-                    try:
-                        if issubclass(item, Command):  
-                            self.commandHandler.registerCommand(folder, item())
-                    except TypeError:
-                        continue 
-                
+        for pluginFolder in os.listdir(plugins_path):
+            if os.path.isdir(os.path.join(plugins_path, pluginFolder)):
+                #TRY import
+                try:
+                    plugin_module = importlib.import_module(f"{plugins_pkg}.{pluginFolder}")
+                    for itemName in dir(plugin_module):
+                        item = getattr(plugin_module, itemName)
+                        #TRY REGISTER
+                        try: 
+                            if isinstance(item, type) and issubclass(item, Command) and item is not Command:
+                                self.commandHandler.registerCommand(pluginFolder, item())
+                                logging.info("Registered " + pluginFolder + " From " + itemName + " Plugin.")
+                        except TypeError:
+                            continue
+                except ImportError as e:
+                    logging.error(f"Error Importing Plugin {pluginFolder}: {e}")
+
+    def start(self):
+        self.enable_plugins()
+        logging.info("Application started.")
         print("Type \"exit\" to exit.")
         while True:
             userInput = input(">>> ").strip()
@@ -33,9 +56,12 @@ class App:
                 operands = userInput[1:len(userInput)]
                 self.commandHandler.executeCommand(command, operands)
 
-            except IndexError: # if no arguments or missing arguments
-                print("Arguments were missing: Use 'menu' to see proper formating.")
-            except TypeError:
-                print ("Invalid/Missing arguments: Use 'menu' to see proper formatting.")
-            except Exception as e:
+            except IndexError: # if no arguments or missing arguments --> happens when just one argument missing or no command
+                logging.warning("Command or Required Arguments Missing.")
+                print("Command or Required Arguments Missing: Use 'menu' to see proper formating.")
+            except TypeError: # happens when just typing operation
+                logging.warning("Required Arguments were Missing.")
+                print ("Required Arguments Missing: Use 'menu' to see proper formatting.")
+            except Exception as e: # just incase
+                logging.error("Unhandled Error: " + str(e))
                 print("Unhandled Error: " + str(e))
